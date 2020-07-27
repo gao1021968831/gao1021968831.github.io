89e0676c728b5edb68e9d360c8ef4602c1b18f84
gao1021968831 <1021968831@qq.com>
2020-07-27 08:46:56 +0800

    Site updated: 2020-07-27 08:46:54

--- js/tw_cn.js
+++ js/tw_cn.js
@@ -96,6 +96,3 @@ function translateInitilization () {
 (function () {
   translateInitilization()
 })()
-$(document).ready(function(e){
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i>崽崽~');
-})
